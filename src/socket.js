8566fcdab7f77f61f697c6e02467de5ebe48bee4
Vedant Jain <jscafe.business@gmail.com>
2023-09-18 17:52:07 +0530

    Provide BE endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-
-export const socket = io('http://localhost:5000');
+const URL = process.env.NODE_ENV === 'production' ? 'https://jscafe-sketchbook-server.onrender.com' : 'http://localhost:5000'
+export const socket = io(URL);
