ca9fc24ceb4d50bd3f52ebc21c32956350302f04
Vedant Jain <jscafe.business@gmail.com>
2023-09-18 17:43:49 +0530

    send and receive events

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -2,7 +2,9 @@ import { useEffect, useLayoutEffect, useRef } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 
 import { MENU_ITEMS } from "@/constants";
-import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
+import { actionItemClick } from '@/slice/menuSlice'
+
+import { socket } from "@/socket";
 
 const Board = () => {
     const dispatch = useDispatch()
@@ -39,11 +41,21 @@ const Board = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d')
 
-        const changeConfig = () => {
+        const changeConfig = (color, size) => {
             context.strokeStyle = color
             context.lineWidth = size
         }
-        changeConfig()
+
+        const handleChangeConfig = (config) => {
+            console.log("config", config)
+            changeConfig(config.color, config.size)
+        }
+        changeConfig(color, size)
+        socket.on('changeConfig', handleChangeConfig)
+
+        return () => {
+            socket.off('changeConfig', handleChangeConfig)
+        }
     }, [color, size])
 
     // before browser pain
@@ -67,11 +79,13 @@ const Board = () => {
         const handleMouseDown = (e) => {
             shouldDraw.current = true
             beginPath(e.clientX, e.clientY)
+            socket.emit('beginPath', {x: e.clientX, y: e.clientY})
         }
 
         const handleMouseMove = (e) => {
             if (!shouldDraw.current) return
             drawLine(e.clientX, e.clientY)
+            socket.emit('drawLine', {x: e.clientX, y: e.clientY})
         }
 
         const handleMouseUp = (e) => {
@@ -81,14 +95,28 @@ const Board = () => {
             historyPointer.current = drawHistory.current.length - 1
         }
 
+        const handleBeginPath = (path) => {
+            beginPath(path.x, path.y)
+        }
+
+        const handleDrawLine = (path) => {
+            drawLine(path.x, path.y)
+        }
+
         canvas.addEventListener('mousedown', handleMouseDown)
         canvas.addEventListener('mousemove', handleMouseMove)
         canvas.addEventListener('mouseup', handleMouseUp)
+
+        socket.on('beginPath', handleBeginPath)
+        socket.on('drawLine', handleDrawLine)
 
         return () => {
             canvas.removeEventListener('mousedown', handleMouseDown)
             canvas.removeEventListener('mousemove', handleMouseMove)
             canvas.removeEventListener('mouseup', handleMouseUp)
+
+            socket.off('beginPath', handleBeginPath)
+            socket.off('drawLine', handleDrawLine)
         }
     }, [])
 
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -5,6 +5,7 @@ import styles from './index.module.css'
 
 import { COLORS, MENU_ITEMS } from '@/constants'
 import {changeColor, changeBrushSize } from '@/slice/toolboxSlice'
+import { socket } from "@/socket";
 
 const Toolbox = () => {
     const dispatch = useDispatch()
@@ -12,13 +13,17 @@ const Toolbox = () => {
     const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL
     const showBrushToolOption = activeMenuItem === MENU_ITEMS.PENCIL || activeMenuItem === MENU_ITEMS.ERASER
     const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
+
     const updateBrushSize = (e) => {
         dispatch(changeBrushSize({item: activeMenuItem, size: e.target.value}))
+        socket.emit('changeConfig', {color, size: e.target.value })
     }
 
     const updateColor = (newColor) => {
         dispatch(changeColor({item: activeMenuItem, color: newColor}))
+        socket.emit('changeConfig', {color: newColor, size })
     }
+    
     return (<div className={styles.toolboxContainer}>
         {showStrokeToolOption && <div className={styles.toolItem}>
             <h4 className={styles.toolText}>Stroke Color</h4>
